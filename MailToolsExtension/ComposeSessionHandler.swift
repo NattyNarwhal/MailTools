63ea1efc9dc4804378aa478ab811c9e82f2ff149
Calvin Buckley <calvin@cmpct.info>
2024-09-29 19:53:00 -0300

    Basic UI manual overrides
    We should probably add more settings, and more finer grained control.
    Putting global settings in here seems unwieldy, can we use the .app for
    this?
    
    Oh yeah, and also defaults.

--- MailToolsExtension/ComposeSessionHandler.swift
+++ MailToolsExtension/ComposeSessionHandler.swift
@@ -11,7 +11,11 @@ import os
 
 fileprivate let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "ComposeSessionHandler")
 
-class ComposeSessionHandler: NSObject, MEComposeSessionHandler {
+class ComposeSessionHandler: NSObject, MEComposeSessionHandler, ObservableObject {
+    @Published var checkHtml = true
+    @Published var checkTopPosting = true
+    @Published var checkColumnSize = true
+    @Published var maxColumnSize: Int = 72
 
     func mailComposeSessionDidBegin(_ session: MEComposeSession) {
         logger.debug("Start session")
@@ -34,7 +38,7 @@ class ComposeSessionHandler: NSObject, MEComposeSessionHandler {
     func allowMessageSendForSession(_ session: MEComposeSession, completion: @escaping (Error?) -> Void) {
         logger.debug("Allow message send?")
         
-        // TODO: Check to see if we should be even checking against this email in the first place
+        // TODO: Should we aggregate multiple issues?
         
         do {
             let parser = try Parser(session: session)
@@ -48,16 +52,17 @@ class ComposeSessionHandler: NSObject, MEComposeSessionHandler {
             print(parser.getLines())
 #endif
             
-            if !parser.isPlainText() {
+            if self.checkHtml && !parser.isPlainText() {
                 throw NSError(mailToolsMessage: "The email should be plain text. Go to Format -> Make Plain Text to make this email no longer HTML.")
             }
             
-            let exceedingLines = parser.linesThatExceed(columns: 72)
-            if let exceedingLine = exceedingLines.first {
-                throw NSError(mailToolsMessage: "The line \"\(exceedingLine.text)\" is longer than 72 characters.")
+            let exceedingLines = parser.linesThatExceed(columns: maxColumnSize)
+            // Only display the first line since the rest will probably be obvious from there
+            if self.checkColumnSize, let exceedingLine = exceedingLines.first {
+                throw NSError(mailToolsMessage: "The line \"\(exceedingLine.text)\" is longer than \(maxColumnSize) characters.")
             }
             
-            if parser.isTopPosting() {
+            if self.checkTopPosting && parser.isTopPosting() {
                 throw NSError(mailToolsMessage: "The reply is written at the beginning of the email. Move your reply inline or below the quote.")
             }
             
--- MailToolsExtension/ComposeSessionView.swift
+++ MailToolsExtension/ComposeSessionView.swift
@@ -10,10 +10,35 @@ import MimeParser
 import MailKit
 
 struct ComposeSessionView: View {
-    var sessionHandler: ComposeSessionHandler!
+    @ObservedObject var sessionHandler: ComposeSessionHandler
     
     var body: some View {
         VStack(alignment: .leading) {
+            Form {
+                Section {
+                    Toggle(isOn: $sessionHandler.checkHtml) {
+                        Text("Check if email isn't plain text")
+                    }
+                    Toggle(isOn: $sessionHandler.checkTopPosting) {
+                        Text("Check if replies are top posting")
+                    }
+                }
+                Section {
+                    // TODO: Language here sucks
+                    Toggle(isOn: $sessionHandler.checkColumnSize) {
+                        Text("Check if line exceeds column limit")
+                    }
+                    TextField("Lines can't exceed column", value: $sessionHandler.maxColumnSize, format: .number)
+                        .disabled(!sessionHandler.checkColumnSize)
+                }
+            }
+            .modify {
+                if #available(macOS 13, *) {
+                    $0.formStyle(.grouped)
+                } else {
+                    $0.padding(7)
+                }
+            }
         }
     }
 }
