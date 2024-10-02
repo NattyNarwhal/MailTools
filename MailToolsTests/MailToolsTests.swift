a5049688c27651b866e0ff9464e48031b8b7eb96
Calvin Buckley <calvin@cmpct.info>
2024-10-02 02:50:47 -0300

    improved tests

--- MailToolsTests/MailToolsTests.swift
+++ MailToolsTests/MailToolsTests.swift
@@ -9,9 +9,45 @@
 import Testing
 
 struct ParserTests {
+    
+    @Test func isPlainText() async throws {
+        let html = """
+        <html>
+         <head></head>
+         <body style="overflow-wrap: break-word; -webkit-nbsp-mode: space; line-break: after-white-space;">
+          text/html
+         </body>
+        </html>
+        """
+        let parser = try MailParser(html: html)
+        #expect(!parser.isPlainText())
+    }
+    
+    @Test func exceedsColumnLimit() async throws {
+        let html = """
+        <html>
+         <head></head>
+         <body style="overflow-wrap: break-word; -webkit-nbsp-mode: space; line-break: after-white-space;" class="ApplePlainTextBody">
+          <div>
+           text/html
+          </div>
+          <div>
+           Test...
+          </div>
+          <div>
+           <br />
+          </div>
+          <div>
+           abcefghijklmnopqrstuvqxyzabcefghijklmnopqrstuvqxyzabcefghijklmnopqrstuvqxyz
+          </div>
+         </body>
+        </html>
+        """
+        let parser = try MailParser(html: html)
+        #expect(!parser.linesThatExceed(columns: 72).isEmpty)
+    }
 
-    @Test func checkTopPost() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test func topPosting() async throws {
         let html = """
         <html>
          <head></head>
